d1849329656e6836fb0e6697f370d716d37ce99e
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-04-11 08:26:22 +0900

    up

--- pysta3/_tmp.py
+++ pysta3/_tmp.py
@@ -1,13 +1,16 @@
 if sys.platform == 'ios':
   import sys
-  input_text: str = ''
-  # todo: そもそも、input text が存在するか確認した方がいいか
   from pathlib import Path
+  
+  # todo: そもそも、input text が存在するか確認した方がいいか
+  input_text: str = ''
   question_path = Path('./input_file.txt')
+  
   if question_path.exists():
     input_text = question_path.read_text()
   else:
     import clipboard
+    
     input_text = clipboard.get()
     question_path.write_text(input_text, encoding='utf-8')
 
