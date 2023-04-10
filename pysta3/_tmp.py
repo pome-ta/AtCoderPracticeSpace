0342063fb1258cab1a8ace4e68bb45ad6622feb4
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-04-10 14:37:40 +0900

    _tmp.py

--- pysta3/_tmp.py
+++ pysta3/_tmp.py
@@ -2,8 +2,11 @@ import sys
 if sys.platform == 'ios':
   # todo: そもそも、問題文が存在するか確認した方がいいか
   from pathlib import Path
-  import clipboard
-  clipboard_text = clipboard.get()
+  question_path = Path('./input_file.txt')
+  if not question_path.exists():
+    import clipboard
+    clipboard_text = clipboard.get()
+    
   clipboard_text = clipboard_text.split('\n')
   text = '\n'.join(clipboard_text)
   with open('input_file.txt', 'w') as f:
