dab014be3537febc243251749206f4ed47948a4e
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-04-12 02:52:36 +0900

    up

---
+++ pysta3/01_AtCoderBeginnersSelection/ABC081A/230412_0250.py
@@ -0,0 +1,14 @@
+import sys
+
+if sys.platform == 'ios':
+  from pathlib import Path
+  input_example_path = Path('./input_file.txt')
+
+  if not input_example_path.exists():
+    import clipboard
+    input_text = clipboard.get()
+    input_example_path.write_text(input_text, encoding='utf-8')
+  sys.stdin = open(input_example_path)
+
+
+v = input()
