b0002899cd471d491623ca6fc7cd62c3aba9d3f9
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-04-12 13:31:47 +0900

    ac

---
+++ pysta3/01_AtCoderBeginnersSelection/ABC081A/230412_1326.py
@@ -0,0 +1,17 @@
+import sys
+
+if sys.platform == 'ios':
+  from pathlib import Path
+  input_example_path = Path('./input_file.txt')
+
+  if not input_example_path.exists():
+    import clipboard
+    input_text = clipboard.get()
+    input_example_path.write_text(input_text, encoding='utf-8')
+  sys.stdin = open(input_example_path)
+
+#s1, s2, s3 = map(int, list(input()))
+#print(sum(map(int, list(input()))))
+
+print(sum([int(i) for i in input()]))
+
