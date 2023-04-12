e487d22476c0b6d35e6fa4b9ec062afab86650d6
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-04-13 01:51:15 +0900

    up

---
+++ pysta3/01_AtCoderBeginnersSelection/05_ABC087B/230412_2339.py
@@ -0,0 +1,20 @@
+import sys
+
+if sys.platform == 'ios':
+  from pathlib import Path
+  import clipboard
+  
+  _path = Path('./input_file.txt')
+  None if _path.exists() else _path.write_text(
+    clipboard.get(), encoding='utf-8')
+  sys.stdin = open(_path)
+
+# 500 円玉 A 枚
+# 100 円玉 B 枚
+#  50 円玉 C 枚
+# 合計 X 円
+# X 円は、何通り
+
+ratio = [500, 100, 50, 1]
+a, b, c, x = [int(input()) * r for r in ratio]
+
