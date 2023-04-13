e80f54648d2dd38e0cd2ef9c2b4a80d1cc58cf46
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-04-13 11:33:51 +0900

    _tmp.py

--- pysta3/01_AtCoderBeginnersSelection/05_ABC087B/_tmp.py
+++ pysta3/01_AtCoderBeginnersSelection/05_ABC087B/_tmp.py
@@ -2,11 +2,9 @@ import sys
 
 if sys.platform == 'ios':
   from pathlib import Path
-  input_example_path = Path('./input_file.txt')
+  import clipboard
 
-  if not input_example_path.exists():
-    import clipboard
-    input_text = clipboard.get()
-    input_example_path.write_text(input_text, encoding='utf-8')
-  sys.stdin = open(input_example_path)
-
+  _path = Path('./input_file.txt')
+  None if _path.exists() else _path.write_text(
+    clipboard.get(), encoding='utf-8')
+  sys.stdin = open(_path)
