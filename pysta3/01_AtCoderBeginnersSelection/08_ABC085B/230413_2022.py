28340c5ef6eb9b6d317497e15efb61c242a2c6b9
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-04-13 20:23:18 +0900

    init

---
+++ pysta3/01_AtCoderBeginnersSelection/08_ABC085B/230413_2022.py
@@ -0,0 +1,10 @@
+import sys
+
+if sys.platform == 'ios':
+  from pathlib import Path
+  import clipboard
+
+  _path = Path('./input_file.txt')
+  None if _path.exists() else _path.write_text(
+    clipboard.get(), encoding='utf-8')
+  sys.stdin = open(_path)
