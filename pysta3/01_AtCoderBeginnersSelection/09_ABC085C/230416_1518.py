66009b9eb723ef838c5157e22828b6b24d1b9264
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-04-16 15:35:29 +0900

    TLE

---
+++ pysta3/01_AtCoderBeginnersSelection/09_ABC085C/230416_1518.py
@@ -0,0 +1,35 @@
+import sys
+
+if sys.platform == 'ios':
+  from pathlib import Path
+  import clipboard
+
+  _path = Path('./input_file.txt')
+  None if _path.exists() else _path.write_text(
+    clipboard.get(), encoding='utf-8')
+  sys.stdin = open(_path)
+
+# N 枚のお札
+# Y 円のお金
+
+#20 196000
+
+#1000 1234000
+# 1234
+#14 27 959
+
+N, Y = map(int, input().split(' '))
+y = int(Y / 1000)
+r = [-1, -1, -1]
+
+for m in range(N + 1):
+  for g in range(N + 1):
+    for s in range(N + 1):
+      if not (m + g + s == N):
+        continue
+      if (m * 10) + (g * 5) + (s) == y:
+        r = [m, g, s]
+        break
+
+print(r[0], r[1], r[2])
+
