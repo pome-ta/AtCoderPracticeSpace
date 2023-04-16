31660c29456444b4bf1574f64e3217e0042298d0
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-04-17 00:55:58 +0900

    up

--- pysta3/01_AtCoderBeginnersSelection/09_ABC085C/230416_2225.py
+++ pysta3/01_AtCoderBeginnersSelection/09_ABC085C/230416_2225.py
@@ -20,26 +20,30 @@ if sys.platform == 'ios':
 
 # N = m + g + s
 # y = (m * 10) + (g * 5) + (s * 1)
-#input = sys.stdin.readline
-#a = input()
-
-read = sys.stdin.read
-a = read()
 
 
 
-
-'''
 N, Y = map(int, input().split(' '))  # 9
+
+#N, Y = [20, 196000]
+#N, Y = [1000, 1234000]
+#N, Y = [2000, 20000000]
+
 y = int(Y / 1000)  # 45
+
 r = [-1, -1, -1]
 
 #1000
 
-m = N if Y / 10 < N else y / 10
-g = N if y / 5 < N else y / 5
-s = N if y / 1 < N else y / 1
+m = N if y / 10 > N else y / 10
+g = N if y / 5 > N else y / 5
+s = N if y / 1 > N else y / 1
+'''
 
+m = y / 10
+g = y / 5
+s = y / 1
+'''
 aaa = [m, g, s]
 print(sum(aaa))
-'''
+
