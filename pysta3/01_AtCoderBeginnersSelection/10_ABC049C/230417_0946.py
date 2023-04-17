07a0d5461d1317a18b0aefe3f206570dffbf347c
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-04-18 02:08:23 +0900

    wa

--- pysta3/01_AtCoderBeginnersSelection/10_ABC049C/230417_0946.py
+++ pysta3/01_AtCoderBeginnersSelection/10_ABC049C/230417_0946.py
@@ -8,3 +8,28 @@ if sys.platform == 'ios':
   None if _path.exists() else _path.write_text(
     clipboard.get(), encoding='utf-8')
   sys.stdin = open(_path)
+
+import re
+
+
+def check_match(_s, _p):
+  for p in _p:
+    if not(_s):
+      break
+    m = re.match(p, _s)
+    x=1
+    if bool(m):
+      e = m.end()
+      _s = _s[e:]
+      check_match(_s, _p)
+  
+  return 'NO' if _s else 'YES'
+
+
+s = input()
+result = 'NO'
+re_matches = [r'^dream', r'^dreamer', r'^erase', r'^eraser']
+compile = map(re.compile, re_matches)
+
+print(check_match(s, compile))
+
