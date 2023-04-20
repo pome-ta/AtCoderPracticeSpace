563526226b529d3470034dd3cf7d989e18bd392c
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-04-20 21:49:33 +0900

    wq

--- pysta3/01_AtCoderBeginnersSelection/11_ABC086C/230420_1428.py
+++ pysta3/01_AtCoderBeginnersSelection/11_ABC086C/230420_1428.py
@@ -8,3 +8,28 @@ if sys.platform == 'ios':
   None if _path.exists() else _path.write_text(
     clipboard.get(), encoding='utf-8')
   sys.stdin = open(_path)
+
+
+def position(t, pxy, axy) -> bool:
+  trgt = sum(pxy)
+  if t + sum(axy) < trgt:
+    return False
+  tmod = True if t % 2 == 0 else False
+  pmod = True if trgt % 2 == 0 else False
+  return True if tmod == pmod else False
+
+
+N = int(input())
+
+a = [0, 0]
+for s in range(N):
+  n, _x, _y = map(int, input().split(' '))
+  p = [_x, _y]
+  result = position(n, p, a)
+  if result:
+    a = p
+  else:
+    break
+
+print('Yes' if result else 'No')
+
