6b3dbb68fcec7c161725f50d71e46faec5c97be6
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-04-25 01:57:24 +0900

    up

--- pysta3/03_typical90/001/230424_1943.py
+++ pysta3/03_typical90/001/230424_1943.py
@@ -8,3 +8,15 @@ if sys.platform == 'ios':
   None if _path.exists() else _path.write_text(
     clipboard.get(), encoding='utf-8')
   sys.stdin = open(_path)
+
+# 左右の長さが L [cm]
+# N 個の切れ目
+# 左から i 番目の切れ目
+# 左から A i​ [cm] の位置
+# N 個の切れ目のうち K 個
+# K+1 個のピースに分割
+
+N, L, K, *As = [
+  int(i) for i in sum([l.split() for l in sys.stdin.readlines()], [])
+]
+
