8c795eac80d9bb5f86453ab0b2504ff721d1da9d
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-04-28 00:48:08 +0900

    up

--- pysta3/03_typical90/002/230427_1938.py
+++ pysta3/03_typical90/002/230427_1938.py
@@ -8,3 +8,37 @@ if sys.platform == 'ios':
   None if _path.exists() else _path.write_text(
     clipboard.get(), encoding='utf-8')
   sys.stdin = open(_path)
+
+
+def get_bracket(n: int) -> str:
+  return ('(' * n) + (')' * n)
+
+
+N = int(input())
+
+is_possible = True if N % 2 == 0 or N else False
+
+result = ''
+
+if is_possible:
+  loop = N // 2
+  #s = ('(' * loop) + (')' * loop)
+  s_list = []
+  for b in range(2):
+    if b:
+      for l in range(loop):
+        s_list.append('()' * l + get_bracket(loop - l))
+    else:
+      for l in range(loop):
+        s_list.append(get_bracket(loop - l) + '()' * l)
+
+#print(result)
+'''
+
+((((()))))
+ (((()
+((((()))))
+()(((())))
+()()((()))
+'''
+
